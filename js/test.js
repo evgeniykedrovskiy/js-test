b780c2c652db3a35bb9c1882c761ed238fa21491
evgeniykedrovskiy <samsvetom@gmail.com>
2022-11-01 10:31:06 +0200

    test

---
+++ js/test.js
@@ -0,0 +1,28 @@
+
+
+function calculateTotal(number) {
+ // Change code below this line
+for (let i = 0; i <= number; i += 1) {
+  return i;
+}
+
+
+  // Change code above this line
+}
+
+console.log(calculateTotal(7));
+
+const number = 7;
+for (let i = 0; i <= number; i += 1) {
+  console.log(i);
+}
+
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) { // Change this line
+//   console.log(i);
+// }
+
+
+
